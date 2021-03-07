de90afd68212517057d7d97fc4b9047ce0f7c817
contauto <76818792+contauto@users.noreply.github.com>
2021-03-07 15:37:38 +0300

    autofac

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,15 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,12 +29,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>>GetAll()
         {
-            if (DateTime.Now.Hour == 1)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
-
-
+           
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
 
@@ -49,11 +48,12 @@ namespace Business.Concrete
             return new SuccessDataResult<List<ProductDetailDto>>( _productDal.GetProductDetails());
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
+
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length < 2)
-            { return new ErrorResult(Messages.ProductNameInvalid); 
-            }
+            
+
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
